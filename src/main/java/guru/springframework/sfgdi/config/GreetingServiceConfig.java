ef323f3ccbb8f4943cea75a1f86a79502f2ef78b
Chris Yager <chris.yager@octo.us>
2022-10-18 10:17:20 -0400

    introduce englishgreetingrepository. inject into EnglishGreeting Service from config. Create EnglishGreetingRepository in config

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgdi.config;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,31 +10,42 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
     @Profile({"ES", "default"})
     @Bean("i18nService")
-    I18NSpanishService i18NSpanishService() { return new I18NSpanishService();}
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService() { return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
+
     @Primary
     @Bean
-    PrimaryGreetingService primaryGreetingService() { return new PrimaryGreetingService(); }
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
     @Bean
-    ConstructorGreetingService constructorGreetingService()
-    {
+    ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
     }
 
     @Bean
-    PropertyInjectedGreetingService propertyInjectedGreetingService()
-    {
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
         return new PropertyInjectedGreetingService();
     }
 
     @Bean
-    SetterInjectedGreetingService setterInjectedGreetingService()
-    {
+    SetterInjectedGreetingService setterInjectedGreetingService() {
         return new SetterInjectedGreetingService();
     }
 }
--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,11 +1,20 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+
 /**
  * Created by jt on 12/27/19.
  */
 public class I18nEnglishGreetingService implements GreetingService {
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
+    private final EnglishGreetingRepository englishGreetingRepository;
     @Override
     public String sayGreeting() {
-        return "Hello World - EN";
+        return this.englishGreetingRepository.sayGreeting();
     }
 }
